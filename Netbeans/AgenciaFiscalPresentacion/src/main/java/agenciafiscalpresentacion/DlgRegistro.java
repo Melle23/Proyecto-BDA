956c06aca88977cbc671a967b2eac46bf6ab8329
RobertoFavela <160526441+RobertoFavela@users.noreply.github.com>
2024-04-06 07:30:23 -0700

    Diseño pantalla registro
    Cambie el diseño de la pantalla de registro a la del figma, sin mover nada de la logica ni funcionamiento

--- Netbeans/AgenciaFiscalPresentacion/src/main/java/agenciafiscalpresentacion/DlgRegistro.java
+++ Netbeans/AgenciaFiscalPresentacion/src/main/java/agenciafiscalpresentacion/DlgRegistro.java
@@ -50,25 +50,18 @@ public class DlgRegistro extends javax.swing.JDialog {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
         campoRFC = new javax.swing.JTextField();
         campoNombre = new javax.swing.JTextField();
         campoApellidoP = new javax.swing.JTextField();
         campoApellidoM = new javax.swing.JTextField();
         campoTelefono = new javax.swing.JTextField();
-        jLabel6 = new javax.swing.JLabel();
         BotonRegistro = new javax.swing.JButton();
         BotonRegistro1 = new javax.swing.JButton();
-        jLabel7 = new javax.swing.JLabel();
         campoCurp = new javax.swing.JTextField();
         BotonLimpiar = new javax.swing.JButton();
         BotonRegreso1 = new javax.swing.JButton();
         fecha = new com.toedter.calendar.JDateChooser();
+        jLabel8 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("REGISTRO");
@@ -76,134 +69,98 @@ public class DlgRegistro extends javax.swing.JDialog {
         jPanel1.setBackground(new java.awt.Color(235, 250, 239));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel2.setBackground(new java.awt.Color(204, 255, 204));
-        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jLabel1.setText("RFC:");
-        jLabel1.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
-        jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 0, -1, 24));
-
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jLabel2.setText("Nombre:");
-        jLabel2.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
-        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 50, -1, 24));
-
-        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jLabel3.setText("Apellido paterno:");
-        jLabel3.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
-        jPanel2.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 90, -1, 24));
-
-        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jLabel4.setText("Apellido materno:");
-        jLabel4.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
-        jPanel2.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 150, -1, 24));
-
-        jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jLabel5.setText("Telefono:");
-        jLabel5.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
-        jPanel2.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 200, -1, 24));
-
         campoRFC.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyTyped(java.awt.event.KeyEvent evt) {
                 campoRFCKeyTyped(evt);
             }
         });
-        jPanel2.add(campoRFC, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 30, 219, -1));
+        jPanel1.add(campoRFC, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 80, 219, -1));
 
         campoNombre.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyTyped(java.awt.event.KeyEvent evt) {
                 campoNombreKeyTyped(evt);
             }
         });
-        jPanel2.add(campoNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 70, 219, -1));
+        jPanel1.add(campoNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 110, 219, -1));
 
         campoApellidoP.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyTyped(java.awt.event.KeyEvent evt) {
                 campoApellidoPKeyTyped(evt);
             }
         });
-        jPanel2.add(campoApellidoP, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 120, 219, -1));
+        jPanel1.add(campoApellidoP, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 150, 219, -1));
 
         campoApellidoM.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyTyped(java.awt.event.KeyEvent evt) {
                 campoApellidoMKeyTyped(evt);
             }
         });
-        jPanel2.add(campoApellidoM, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 180, 219, -1));
+        jPanel1.add(campoApellidoM, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 190, 219, -1));
 
         campoTelefono.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyTyped(java.awt.event.KeyEvent evt) {
                 campoTelefonoKeyTyped(evt);
             }
         });
-        jPanel2.add(campoTelefono, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 230, 219, -1));
+        jPanel1.add(campoTelefono, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 230, 219, -1));
 
-        jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jLabel6.setText("Fecha Nacimiento:");
-        jPanel2.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 320, -1, -1));
-
-        BotonRegistro.setBackground(new java.awt.Color(51, 153, 0));
-        BotonRegistro.setFont(new java.awt.Font("Segoe UI Black", 1, 18)); // NOI18N
-        BotonRegistro.setForeground(new java.awt.Color(255, 255, 255));
-        BotonRegistro.setText("Guardar y regresar");
+        BotonRegistro.setOpaque(false);
+        BotonRegistro.setContentAreaFilled(false);
+        BotonRegistro.setBorderPainted(false);
         BotonRegistro.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 BotonRegistroActionPerformed(evt);
             }
         });
-        jPanel2.add(BotonRegistro, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 360, 260, 40));
+        jPanel1.add(BotonRegistro, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 440, 160, 50));
 
-        BotonRegistro1.setBackground(new java.awt.Color(51, 153, 0));
-        BotonRegistro1.setFont(new java.awt.Font("Segoe UI Black", 1, 18)); // NOI18N
-        BotonRegistro1.setForeground(new java.awt.Color(255, 255, 255));
-        BotonRegistro1.setText("Guardar y solicitar licencia");
+        BotonRegistro1.setOpaque(false);
+        BotonRegistro1.setContentAreaFilled(false);
+        BotonRegistro1.setBorderPainted(false);
         BotonRegistro1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 BotonRegistro1ActionPerformed(evt);
             }
         });
-        jPanel2.add(BotonRegistro1, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 360, -1, 42));
-
-        jLabel7.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jLabel7.setText("CURP:");
-        jPanel2.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 260, -1, -1));
+        jPanel1.add(BotonRegistro1, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 440, 160, 50));
 
         campoCurp.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyTyped(java.awt.event.KeyEvent evt) {
                 campoCurpKeyTyped(evt);
             }
         });
-        jPanel2.add(campoCurp, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 280, 220, -1));
+        jPanel1.add(campoCurp, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 260, 220, -1));
 
-        BotonLimpiar.setBackground(new java.awt.Color(153, 255, 102));
-        BotonLimpiar.setFont(new java.awt.Font("Segoe UI Black", 1, 8)); // NOI18N
-        BotonLimpiar.setText("Limpiar");
+        BotonLimpiar.setOpaque(false);
+        BotonLimpiar.setContentAreaFilled(false);
+        BotonLimpiar.setBorderPainted(false);
         BotonLimpiar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 BotonLimpiarActionPerformed(evt);
             }
         });
-        jPanel2.add(BotonLimpiar, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 320, 70, 30));
+        jPanel1.add(BotonLimpiar, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 390, 90, 40));
 
-        BotonRegreso1.setBackground(new java.awt.Color(153, 255, 102));
-        BotonRegreso1.setFont(new java.awt.Font("Segoe UI Black", 1, 8)); // NOI18N
-        BotonRegreso1.setText("Volver");
+        BotonRegreso1.setOpaque(false);
+        BotonRegreso1.setContentAreaFilled(false);
+        BotonRegreso1.setBorderPainted(false);
         BotonRegreso1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 BotonRegreso1ActionPerformed(evt);
             }
         });
-        jPanel2.add(BotonRegreso1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 70, 30));
-        jPanel2.add(fecha, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 320, -1, -1));
+        jPanel1.add(BotonRegreso1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 450, 60, 50));
+        jPanel1.add(fecha, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 300, -1, -1));
 
-        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 10, 620, 410));
+        jLabel8.setIcon(new javax.swing.ImageIcon("C:\\Users\\favel\\OneDrive\\Documentos\\NetBeansProjects\\Proyecto-BDA\\Netbeans\\AgenciaFiscalPresentacion\\src\\main\\java\\Imagenes\\Pantalla_Registro.png")); // NOI18N
+        jLabel8.setText(" ");
+        jPanel1.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 700, 500));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 698, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -409,14 +366,7 @@ private int calcularEdad() {
     private javax.swing.JTextField campoRFC;
     private javax.swing.JTextField campoTelefono;
     private com.toedter.calendar.JDateChooser fecha;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
 }
